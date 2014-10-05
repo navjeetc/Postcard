ffeb494915eddce25044b49b8153ce5cfbcc594f
Navjeet <navjeetc@gmail.com>
2014-10-05 00:50:51 -0400

    added a label for name field and added logic to populate that label on send mail button click

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     
     @IBOutlet weak var enterMessageTextField: UITextField!
@@ -34,6 +36,12 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterNameTextField.text = ""
